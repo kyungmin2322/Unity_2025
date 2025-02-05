0952df9a4e9e6273c0c3c146875eca4b41d1f212
kyungmin2322 <kyungmin2322@gmail.com>
2025-02-05 11:32:52 +0900

    20250205 11:32
    과녁 맞추기
    Geneator, Shooter 스크립트 작성

--- 20250205/Assets/Scripts/ObjectGenerator.cs
+++ 20250205/Assets/Scripts/ObjectGenerator.cs
@@ -2,13 +2,10 @@ using UnityEngine;
 
 public class ObjectGenerator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    // 1.   이 클래스는 오브젝트를 생성하는 기능을 가지고 있습니다.
+    // 2.   마우스 버튼을 눌렀을 때, 카메라의 스크린 지점을 통해
+    //      물체의 방향을 설정합니다.
+    // 3.   물체를 방향에 맞춰 발사하는 기능을 호출해옵니다.
     void Update()
     {
         
---
+++ 20250205/Assets/Scripts/ObjectShooter.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ObjectShooter : MonoBehaviour
+{
+    // 발사 기능을 제공해주는 클래스
+    // 충돌 시 오브젝트를 고정시켜주는 역할도 진행합니다.
+
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// 물체를 발사하는 기능을 가진 함수(메소드)
+    /// </summary>
+    /// <param name="direction">물체의 발사 방향</param>
+
+    void Shoot(Vector3 direction)
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
