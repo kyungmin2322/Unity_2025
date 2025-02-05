891f3743bd5032e8be5951816d513d035a23258c
kyungmin2322 <kyungmin2322@gmail.com>
2025-02-05 13:37:06 +0900

    20250205 13:37

--- 20250205/Assets/Scripts/ObjectGenerator.cs
+++ 20250205/Assets/Scripts/ObjectGenerator.cs
@@ -6,8 +6,44 @@ public class ObjectGenerator : MonoBehaviour
     // 2.   마우스 버튼을 눌렀을 때, 카메라의 스크린 지점을 통해
     //      물체의 방향을 설정합니다.
     // 3.   물체를 방향에 맞춰 발사하는 기능을 호출해옵니다.
+
+    public GameObject prefab; // 오브젝트 프리팹 등록
+    public float power = 1000f;
+
     void Update()
     {
-        
+        // ~down : 클릭 시 1번
+        // ~up : 버튼을 놓았을 때 1번
+        // : 클릭하고 있는 동안 지속
+
+        // 마우스 0번 버튼을 눌렀을 때
+        // 마우스 버튼
+        // 0 : 왼쪽
+        // 1 : 오른쪽
+        // 2 : 휠
+        if(Input.GetMouseButtonDown(0))
+        {
+            var thrown = Instantiate(prefab);
+            // + as GameObject : Instantiate와 같이 사용하면
+            // 게임 오브젝트로써 생성하라는 의미.
+
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // 레이(Ray)
+            // 가상의 레이저 선으로, 발사되는 시작 지점과 방향
+            // 일반적인 Ray는 Vector3의 값을 가지고, Ray2D의 경우는 Vector2의 값을 가지게 된다.
+
+            // 일반적인 레이 만드는 방법
+            Vector3 origin = new Vector3(0, 0, 0);
+            Vector3 vect_dir = Vector3.forward;
+            Ray newRay = new Ray(origin, vect_dir);
+
+            // 레이는 데이터만 가지고 있는 구조체이므로, 기능적으로는 할 수 있는것이 없으며
+            // Ray를 이용한 방향 계산이나 RayCast를 이용한 오브젝트 충돌 기능으로 활용한다.
+
+            Vector3 direction = ray.direction; // 방향 설정
+
+            thrown.GetComponent<ObjectShooter>().Shoot(direction.normalized * power);
+
+        }
     }
 }
--- 20250205/Assets/Scripts/ObjectShooter.cs
+++ 20250205/Assets/Scripts/ObjectShooter.cs
@@ -1,27 +1,42 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class ObjectShooter : MonoBehaviour
 {
     // 발사 기능을 제공해주는 클래스
     // 충돌 시 오브젝트를 고정시켜주는 역할도 진행합니다.
 
+    GameObject objectGenerator;
+    // ObjectGenerator obj;
+
     void Start()
     {
-        
+        objectGenerator = GameObject.Find("ObjectGenerator");
+        // == 오브젝트 탐색 기능 ==
+        // 씬에서 해당 이름을 가진 오브젝트를 찾아 그 값을 얻어오는 GameObject.Find() 기능
+        // objectGenerator = GameObject.FindWithTag("Generator"); // Generator 태그를 가진 오브젝트를 탐색
+        // obj = GameObject.FindObjectOfType<ObjectGenerator>(); // <> 안에 넣어준 타입에 맞는 오브젝트를 탐색
+
+        // 가장 쉬운 건 Find()
+        // 하지만, 검색 범위가 너무 많아지면 불필요하게 성능 저하가 발생할 수 있음.
+        // 따라서, 그 때부터 Tag나 Type 등으로 검색 범위를 제한해서 찾는 방식을 사용.
+
+        // 씬에 해당 값이 없으면 null
+
     }
 
     /// <summary>
     /// 물체를 발사하는 기능을 가진 함수(메소드)
     /// </summary>
     /// <param name="direction">물체의 발사 방향</param>
-
-    void Shoot(Vector3 direction)
+    public void Shoot(Vector3 direction)
     {
-
+        GetComponent<Rigidbody>().AddForce(direction);
+        Debug.Log("맞았습니다!");
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        
+        GetComponent<Rigidbody>().isKinematic = true;
     }
 }
