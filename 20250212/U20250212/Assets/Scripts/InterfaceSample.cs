0a37caa0b9424d74475c9bf36b5714d1133b857c
kyungmin2322 <kyungmin2322@gmail.com>
2025-02-12 09:08:30 +0900

    20250212 09:08
    강의 시작

---
+++ 20250212/U20250212/Assets/Scripts/InterfaceSample.cs
@@ -0,0 +1,17 @@
+// 유니티의 인터페이스(interface)
+
+using UnityEngine;
+
+public class InterfaceSample : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
