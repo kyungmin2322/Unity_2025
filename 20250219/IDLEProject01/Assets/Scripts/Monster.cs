f23eee4be7b6f88f9c43661b4aa5d4612f037459
kyungmin2322 <kyungmin2322@gmail.com>
2025-02-19 10:51:42 +0900

    20250219 10:51 몬스터 이동 스크립트

--- 20250219/IDLEProject01/Assets/Scripts/Monster.cs
+++ 20250219/IDLEProject01/Assets/Scripts/Monster.cs
@@ -1,16 +1,31 @@
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Monster : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float monster_speed;
+    Animator animator;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 영점 기준으로 시선 변경
+        transform.LookAt(Vector3.zero);
+
+        // 영점 기준으로 몬스터의 속도만큼 앞으로 이동
+        transform.position = Vector3.MoveTowards(transform.position,
+            Vector3.zero, Time.deltaTime * monster_speed);
+
+        SetMotionChange("isMOVE", true);
+    }
+
+    private void SetMotionChange(string motion_name, bool param)
+    {
+        animator.SetBool(motion_name, param);
     }
 }
