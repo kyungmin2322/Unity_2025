1eb088ff9a8f66fabed69d72f100be0a359bd936
kyungmin2322 <kyungmin2322@gmail.com>
2025-02-19 12:20:15 +0900

    20250219 12:19 몬스터 생성 완료 몬스터 이동 로직 구현 완료 오브젝트 풀링을 통해 생성 방식에 대한 개선 진행

--- 20250219/IDLEProject01/Assets/Scripts/Monster.cs
+++ 20250219/IDLEProject01/Assets/Scripts/Monster.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Monster : MonoBehaviour
 {
-    public float monster_speed;
     Animator animator;
+
+    public float monster_speed;
+    public float rate = 0.5f;
 
     void Start()
     {
@@ -17,11 +19,22 @@ public class Monster : MonoBehaviour
         // 영점 기준으로 시선 변경
         transform.LookAt(Vector3.zero);
 
-        // 영점 기준으로 몬스터의 속도만큼 앞으로 이동
-        transform.position = Vector3.MoveTowards(transform.position,
-            Vector3.zero, Time.deltaTime * monster_speed);
+        // 간격 설정
+        float target_distance = Vector3.Distance(transform.position,
+            Vector3.zero);
 
-        SetMotionChange("isMOVE", true);
+        if (target_distance <= rate) // 간격 거리와 가까워지면 이동 중지
+        {
+            SetMotionChange("isMOVE", false);
+        }
+        else // 일반적인 경우에는 움직임을 진행
+        {
+            // 영점 기준으로 몬스터의 속도만큼 앞으로 이동
+            transform.position = Vector3.MoveTowards(transform.position,
+                Vector3.zero, Time.deltaTime * monster_speed);
+
+            SetMotionChange("isMOVE", true);
+        }
     }
 
     private void SetMotionChange(string motion_name, bool param)
--- 20250219/IDLEProject01/Assets/Scripts/Spawner.cs
+++ 20250219/IDLEProject01/Assets/Scripts/Spawner.cs
@@ -1,16 +1,46 @@
+using System.Collections;
 using UnityEngine;
 
+// 맵에 특정 마릿수의 몬스터를 설정한 시간마다 반복해서 소환
 public class Spawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+
+    public GameObject monster_prefab;
+    public int monster_count;
+    public float monster_spawn_time;
+
+    // 해당 수치를 수정할 경우,
+    // 생성되는 영역(구)의 위치 값이 점점 넓어진다.
+    public float summon_rate = 5.0f; 
+    // 생성 위치를 기준으로 생성되는 영역(구)를 설정할 수 있다.
+    public float re_rate = 2.0f;
+
     void Start()
     {
-        
+        StartCoroutine("SpawnMonster");
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnMonster()
     {
-        
+        Vector3 pos; // 생성 좌표
+
+        for(int i = 0; i < monster_count; i++)
+        {
+            pos = Vector3.zero + Random.insideUnitSphere * summon_rate;
+            pos.y = 0.0f; // 생성된 유닛이 맵에 제대로 존재하기 위해 설정
+
+            // 플레이어와 근접한 범위에서 생성됐을 경우 재할당
+            while(Vector3.Distance(pos, Vector3.zero) <= re_rate)
+            {
+                pos = Vector3.zero + Random.insideUnitSphere * summon_rate;
+                pos.y = 0.0f;
+            }
+
+            GameObject go = Instantiate(monster_prefab, pos,
+                Quaternion.identity);
+        }
+        yield return new WaitForSeconds(monster_spawn_time);
+        StartCoroutine("SpawnMonster");
     }
 }
