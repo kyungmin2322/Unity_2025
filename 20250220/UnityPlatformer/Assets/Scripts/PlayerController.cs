cd9d49e37f3a517cdd36e747b871cfa81084b319
kyungmin2322 <kyungmin2322@gmail.com>
2025-02-20 09:48:02 +0900

    20250220 09:47 맵 배치, 레이어&태그 설정, 콜라이더 배치

---
+++ 20250220/UnityPlatformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rbody;
+
+    float 
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+    
+    }
+}
